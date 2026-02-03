d3b14daec9d2c8a526631daba6b758d13e6286bc
Gyges <shepherd2sovereign@gmail.com>
2026-02-03 08:55:37 -0500

    dataset and table getters

--- examples/bq_test.rs
+++ examples/bq_test.rs
@@ -1,18 +1,18 @@
 use log::info;
-use schema_searcher::bigquery::{client::authenticate, types::DatasetList};
+use schema_searcher::bigquery::{
+    client::{authenticate, get_datasets, get_tables},
+    types::DatasetList,
+};
 
 #[tokio::main]
 async fn main() -> Result<(), Box<dyn std::error::Error>> {
     let client = authenticate("zw_personal_service_acc.json").await?;
 
-    let datasets = client
-        .get("https://bigquery.googleapis.com/bigquery/v2/projects/bigquery-public-data/datasets/")
-        .send()
-        .await?
-        .json::<DatasetList>()
-        .await?;
+    let project = "bigquery-public-data";
 
-    println!("{:?}", datasets);
+    let tables = get_datasets(&client, project).await.unwrap();
+
+    println!("{:?}", tables);
 
     Ok(())
 }
--- src/bigquery/client.rs
+++ src/bigquery/client.rs
@@ -2,6 +2,8 @@ use log::info;
 use reqwest::Client;
 use yup_oauth2::read_service_account_key;
 
+use crate::bigquery::types::{DatasetList, DatasetReference, TableList, TableReference};
+
 pub async fn authenticate(creds_path: &str) -> Result<Client, Box<dyn std::error::Error>> {
     // GBQ authentication and Reqwest client boilerplate
     let service_account_key: yup_oauth2::ServiceAccountKey =
@@ -27,4 +29,42 @@ pub async fn authenticate(creds_path: &str) -> Result<Client, Box<dyn std::error
         .build()?;
 
     Ok(client)
+}
+
+pub async fn get_datasets(
+    client: &Client,
+    project_id: &str,
+) -> Result<Vec<DatasetReference>, Box<dyn std::error::Error>> {
+    let url = format!(
+        "https://bigquery.googleapis.com/bigquery/v2/projects/{}/datasets",
+        project_id
+    );
+    let response = client.get(&url).send().await?.json::<DatasetList>().await?;
+
+    let datasets = response
+        .datasets
+        .into_iter()
+        .map(|dataset| dataset.dataset_reference)
+        .collect();
+
+    Ok(datasets)
+}
+
+pub async fn get_tables(
+    client: &Client,
+    dataset: &DatasetReference,
+) -> Result<Vec<TableReference>, Box<dyn std::error::Error>> {
+    let url = format!(
+        "https://bigquery.googleapis.com/bigquery/v2/projects/{}/datasets/{}/tables",
+        dataset.project_id, dataset.dataset_id
+    );
+    let response = client.get(&url).send().await?.json::<TableList>().await?;
+
+    let tables = response
+        .tables
+        .into_iter()
+        .map(|table| table.table_reference)
+        .collect();
+
+    Ok(tables)
 }
